689d52ed5ff108b13c83e15807ea1a01a470b85e
VMLit <vincent.lit@nov.com>
2024-10-07 09:14:23 +0200

    Completed first exercises

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,15 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut a = n;
+    let mut fact = 1;
+
+    while a > 0 {
+        fact = a * fact;
+        a -= 1;
+    }
+
+    fact
 }
 
 #[cfg(test)]
